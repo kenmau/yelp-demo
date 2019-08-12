a5db32b28da48f90634b4eb2fb251e41e34ff2dd
Ken Mau <ken.mau@rbc.com>
2019-08-12 12:46:42 -0400

    initial commit

---
+++ Yelp Demo/Yelp Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Yelp Demo
+//
+//  Created by Ken Mau on 2019-08-12.
+//  Copyright © 2019 Ken. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
